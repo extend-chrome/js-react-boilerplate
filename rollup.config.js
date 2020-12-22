8395957df34d1db1ccaa33f5de4831974adfeaa0
Jack Steam <jacksteamdev@gmail.com>
2020-12-22 10:35:00 -0600

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,54 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import alias from '@rollup/plugin-alias'
+import babel from '@rollup/plugin-babel'
+
+import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
+import { emptyDir } from 'rollup-plugin-empty-dir'
+import zip from 'rollup-plugin-zip'
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+// Aliases for module resolution
+const aliases = isProduction
+  ? [
+      {
+        find: 'react',
+        // Use the production build
+        replacement: require.resolve('react/esm/react.production.min.js'),
+      },
+      {
+        find: 'react-dom',
+        // Use the production build
+        replacement: require.resolve(
+          'react-dom/esm/react-dom.production.min.js',
+        ),
+      },
+    ]
+  : []
+
+export default {
+  input: 'src/manifest.json',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    chunkFileNames: 'chunks/[name]-[hash].js',
+  },
+  plugins: [
+    chromeExtension(),
+    // Adds a Chrome extension reloader during watch mode
+    simpleReloader(),
+    alias({ entries: aliases }),
+    babel({
+      // Do not transpile dependencies
+      ignore: ['node_modules'],
+      babelHelpers: 'bundled',
+    }),
+    resolve(),
+    commonjs(),
+    // Empties the output dir before a new build
+    emptyDir(),
+    // Outputs a zip file in ./releases
+    isProduction && zip({ dir: 'releases' }),
+  ],
+}
